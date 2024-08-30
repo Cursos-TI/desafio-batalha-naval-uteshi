f9e0aa55ad94fcab77f1b551bcb0d845ecb10a01
Gabriel Utsch <97922187+uteshi@users.noreply.github.com>
2024-08-30 15:13:20 +0000

    BATALHA-NAVAL-1.0v

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,38 +3,110 @@
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
+#define LINHAS 10
+#define COLUNAS 10
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    //INICIO DESAFIO INICIANTE
+    int CAMPO[LINHAS][COLUNAS] = {0}; //ESCOPO DO CAMPO DO TABULEIRO AGUA = 0
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    //NAVIO 1 - HORIZONTAL
+    CAMPO[0][0] = 3; 
+    CAMPO[0][1] = 3; 
+    CAMPO[0][2] = 3; 
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    //NAVIO 2 - VERTICAL
+    CAMPO[7][9] = 3; 
+    CAMPO[8][9] = 3; 
+    CAMPO[9][9] = 3; 
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    // GERANDO O CAMPO COM OS NAVIOS
+    printf("### Tabuleiro INICIANTE:\n");
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            printf("%d ", CAMPO[i][j]);
+        }
+        printf("\n");
+    }
+    // FIM DESAFIO INICIANTE
+   
+   //INICIO DESAFIO AVENTUREIRO
+    int CAMPO2[LINHAS][COLUNAS] = {0}; //ESCOPO DO CAMPO DO TABULEIRO AGUA = 0
+
+    //NAVIO 1 - HORIZONTAL
+    CAMPO2[0][0] = 3; 
+    CAMPO2[0][1] = 3; 
+    CAMPO2[0][2] = 3; 
+
+    //NAVIO 2 - VERTICAL
+    CAMPO2[7][9] = 3; 
+    CAMPO2[8][9] = 3; 
+    CAMPO2[9][9] = 3; 
+
+    //NAVIO 3 - DIAGONAL
+    CAMPO2[5][6] = 3;
+    CAMPO2[6][5] = 3;
+    CAMPO2[7][4] = 3;
+
+    // GERANDO O CAMPO COM OS NAVIOS
+    printf("\n\n### Tabuleiro AVENTUREIRO:\n");
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            printf("%d ", CAMPO2[i][j]);
+        }
+        printf("\n");
+    }
+    // FIM DESAFIO AVENTUREIRO
+
+    //INICIO DESAFIO MESTRE
+    int CAMPO3[LINHAS][COLUNAS] = {0};
+
+    //HABILIDADE - GOLPE EM CONE
+    CAMPO3[0][2] = 1;
+    CAMPO3[1][1] = 1;
+    CAMPO3[1][2] = 1;
+    CAMPO3[1][3] = 1;
+    CAMPO3[2][0] = 1;
+    CAMPO3[2][1] = 1;
+    CAMPO3[2][2] = 1;
+    CAMPO3[2][3] = 1;
+    CAMPO3[2][4] = 1;
+
+    //HABILIDADE - GOLPE EM OCTAEDRO
+    CAMPO3[2][6] = 1;
+    CAMPO3[3][5] = 1;
+    CAMPO3[3][6] = 1;
+    CAMPO3[3][7] = 1;
+    CAMPO3[4][4] = 1;
+    CAMPO3[4][5] = 1;
+    CAMPO3[4][6] = 1;
+    CAMPO3[4][7] = 1;
+    CAMPO3[4][8] = 1;
+    CAMPO3[5][5] = 1;
+    CAMPO3[5][6] = 1;
+    CAMPO3[5][7] = 1;
+    CAMPO3[6][6] = 1;
+
+    //HABILIDADE - GOLPE EM CRUZ
+    CAMPO3[5][2] = 1;
+    CAMPO3[6][2] = 1;
+    CAMPO3[7][2] = 1;
+    CAMPO3[8][2] = 1;
+    CAMPO3[9][2] = 1;
+    CAMPO3[7][0] = 1;
+    CAMPO3[7][1] = 1;
+    CAMPO3[7][3] = 1;
+    CAMPO3[7][4] = 1;
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // GERANDO O CAMPO COM OS PODERES
+    printf("\n\n### Tabuleiro MESTRE:\n");
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            printf("%d ", CAMPO3[i][j]);
+        }
+        printf("\n");
+    }
+    //FIM DESAFIO MESTRE
 
     return 0;
 }
